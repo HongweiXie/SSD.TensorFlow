update skip net; debug tflite float get wrong result when  have resizebilinear

inference/TFLiteMultiBBoxDetectorWithoutPostProcess.py
@@ -4,17 +4,20 @@ import cv2
 import numpy as np
 import math
 
-#0.0495828
-#169
+#0.0495828  169
+#0.0729403 194
+#0.0500808 163
 class BBox(object):
-    def __init__(self,class_index,score,bbox,anchor):
+    def __init__(self,class_index,score,bbox,anchor,is_quant=True):
         self._class_index=class_index
         self._score=score
-        bbox=self.decode_bbox(bbox, anchor, 0.0495828, 169, 10, 5)
+        self._is_quant = is_quant
+        bbox=self.decode_bbox(bbox, anchor, 0.0500808 , 163, 10, 5)
         self._ymin = max(0,bbox[0])
         self._xmin = max(0,bbox[1])
         self._ymax = min(1,bbox[2])
         self._xmax = min(1,bbox[3])
+
 
 
     def decode_bbox(self, bbox, anchor, q_scale, q_z, xy_scale, hw_scale):
@@ -33,8 +36,8 @@ class BBox(object):
             ymax=y_center+half_h
             xmax=x_center+half_w
             return ymin,xmin,ymax,xmax
-
-        bbox=np.asarray(list(map(dequnt,bbox)))
+        if self._is_quant:
+            bbox=np.asarray(list(map(dequnt,bbox)))
         bbox=decode(bbox)
         return bbox
 
@@ -45,13 +48,14 @@ class BBox(object):
 
 
 class TFLiteMutliBBoxDetectorWithoutPostProcess(object):
-    def __init__(self, tflite_model_file,anchors_file,image_resize_size,threshold=0.01):
+    def __init__(self, tflite_model_file,anchors_file,image_resize_size,threshold=0.01,is_quant=True):
         self._logger = logging.getLogger('MultiBBoxDetector')
         self._logger.setLevel(logging.INFO)
 
         self._logger.info('loading graph from %s' % (tflite_model_file))
         self._image_resize_size=image_resize_size
         self._threshold=threshold
+        self._is_quant=is_quant
 
         self.interpreter = tf.contrib.lite.Interpreter(model_path=str(tflite_model_file))
         self.interpreter.allocate_tensors()
@@ -76,9 +80,10 @@ class TFLiteMutliBBoxDetectorWithoutPostProcess(object):
 
     def inference(self,image):
         image=cv2.resize(image,self._image_resize_size)
-        # image = image.astype(np.float32)
-        # image = image - 127.5
-        # image = image * 0.007843
+        if not self._is_quant:
+            image = image.astype(np.float32)
+            image = image - 127.5
+            image = image * 0.007843
 
         self.interpreter.set_tensor(self.input_index, [image])
         self.interpreter.invoke()
@@ -98,9 +103,12 @@ class TFLiteMutliBBoxDetectorWithoutPostProcess(object):
         for k in range(len(scores)):
             score_tuple = scores[k]
             for i in range(1,num_classes):
-                score=score_tuple[i]/255.
+                if self._is_quant:
+                    score=score_tuple[i]/255.
+                else:
+                    score=score_tuple[i]
                 if score>=threshold and score_tuple[i]>score_tuple[0]:
-                    bbox=BBox(i,score,bboxes[k],self.anchors[k])
+                    bbox=BBox(i,score,bboxes[k],self.anchors[k],self._is_quant)
                     if bbox._xmax>bbox._xmin and bbox._ymax>bbox._ymin:
                         selected_bbox[i].append(bbox)
 

net/BackboneNetwork.py
@@ -3,6 +3,8 @@ slim = tf.contrib.slim
 DEFAULT_PADDING = 'SAME'
 _init_xavier = tf.contrib.layers.xavier_initializer()
 _init_zero = slim.init_ops.zeros_initializer()
+_l2_regularizer_00004 = tf.contrib.layers.l2_regularizer(0.00004)
+
 class BackboneNetwork(object):
 
     def forward(self,inputs, is_training=False):
@@ -12,30 +14,29 @@ class BackboneNetwork(object):
         return tf.image.resize_bilinear(input, [int(input.get_shape()[1]) * factor, int(input.get_shape()[2]) * factor], name=name)
 
     def separable_conv(self, input, k_h, k_w, c_o, stride, name, relu=True, set_bias=True,is_training=True):
-        with slim.arg_scope([slim.batch_norm], decay=0.999, fused=True, is_training=is_training):
-            output = slim.separable_convolution2d(input,
-                                                  num_outputs=None,
-                                                  stride=stride,
-                                                  trainable=is_training,
-                                                  depth_multiplier=1.0,
-                                                  kernel_size=[k_h, k_w],
-                                                  activation_fn=tf.nn.relu6,
-                                                  normalizer_fn=slim.batch_norm,
-                                                  weights_initializer=_init_xavier,
-                                                  padding=DEFAULT_PADDING,
-                                                  scope=name + '_depthwise')
+        with slim.arg_scope([slim.batch_norm], decay=0.97,
+                                epsilon=0.001,
+                                scale=True,
+                                center=True,is_training=is_training):
+            with slim.arg_scope([slim.conv2d, slim.separable_conv2d],
+                                activation_fn=tf.nn.relu6,
+                                normalizer_fn=slim.batch_norm,
+                                weights_regularizer=_l2_regularizer_00004,
+                                weights_initializer=_init_xavier
+                                ):
+                output = slim.separable_convolution2d(input,
+                                                      num_outputs=None,
+                                                      stride=stride,
+                                                      depth_multiplier=1.0,
+                                                      kernel_size=[k_h, k_w],
+                                                      padding=DEFAULT_PADDING,
+                                                      scope=name + '_depthwise')
 
-            output = slim.convolution2d(output,
-                                        c_o,
-                                        stride=1,
-                                        kernel_size=[1, 1],
-                                        activation_fn=tf.nn.relu6,
-                                        weights_initializer=_init_xavier,
-                                        biases_initializer=_init_zero if set_bias else None,
-                                        normalizer_fn=slim.batch_norm,
-                                        trainable=is_training,
-                                        weights_regularizer=None,
-                                        scope=name + '_pointwise')
+                output = slim.convolution2d(output,
+                                            c_o,
+                                            stride=1,
+                                            kernel_size=[1, 1],
+                                            scope=name + '_pointwise')
 
         return output
 

net/mobilenet_v1_ppn_skip_backbone.py
@@ -48,9 +48,12 @@ class MobileNetV1PPNSkipBackbone(BackboneNetwork):
             # conv2d_7_upsample=self.upsample(image_features['Conv2d_7_pointwise'], 2, name='Conv2d_7_pointwise_upsample')
             features_concat=tf.concat([image_features['Conv2d_5_pointwise'],conv2d_11_upsample],axis=3)
             prefix='Feature_Concat'
-            features_concat=self.separable_conv(features_concat,3, 3, min(256,192*4*self._depth_multiplier), 1, name=prefix + '_L_1')
-            features_concat = self.separable_conv(features_concat, 3, 3, 256, 1, name=prefix + '_L_2')
-            # features_concat = self.separable_conv(features_concat, 3, 3, 256, 1, name=prefix + '_L_3')
+            features_concat=self.separable_conv(features_concat,3, 3, min(256,192*4*self._depth_multiplier), 1,is_training=is_training, name=prefix + '_L_1')
+            # features_concat = self.separable_conv(features_concat, 3, 3, 192, 1,
+            #                                       name=prefix + '_L_1')
+
+            features_concat = self.separable_conv(features_concat, 3, 3, min(256,192*4*self._depth_multiplier), 1, is_training=is_training,name=prefix + '_L_2')
+            features_concat = self.separable_conv(features_concat, 3, 3, 256, 1, is_training=is_training,name=prefix + '_L_3')
             # features_concat = self.convb(features_concat, 1, 1, 256, 1, name=prefix + '_L_4')
             with slim.arg_scope([slim.batch_norm],
                                 decay=0.97,